72e6ab687acd6a78639d7bded606df26efa6974b
Johansson-Rodriguez <jrodriguez@solucionesroots.com>
2022-06-12 14:52:44 -0600

    configuration

--- components/Banner.js
+++ components/Banner.js
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div className="h-max md:h-screen md:pt-0 pt-36">
         <img 
-            src="https://scontent.fsal2-1.fna.fbcdn.net/v/t1.6435-9/130303004_152247556644218_6098497004933521068_n.png?_nc_cat=107&ccb=1-5&_nc_sid=e3f864&_nc_ohc=9CwMAb1a8ekAX9rW-yd&_nc_ht=scontent.fsal2-1.fna&oh=00_AT-aAamhmoXzamBVyU5mabKy10TFsPgKjGUDF983MydvuA&oe=6295B8B0"
+            src="https://scontent.fsal2-1.fna.fbcdn.net/v/t1.6435-9/130303004_152247556644218_6098497004933521068_n.png?_nc_cat=107&ccb=1-7&_nc_sid=e3f864&_nc_ohc=in4Fc0KoRxwAX_WTG5E&_nc_ht=scontent.fsal2-1.fna&oh=00_AT_lbTkkQmsPOIShKY_hcb2IF5xHdN6VMYq96j_9YjfxMA&oe=62CD17B0"
             alt="Banner"
             className="object-cover h-full w-full"
         />
